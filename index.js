f9c4d74136a5c3c51f97671ee6248964968994ea
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-03-30 17:19:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Barış Yılmaz';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toLocaleUpperCase
+()}!`
+const shortGreeting = `Welcome, ${currentUser[0]}!`
